9940b749653882169d545d6790e934b3254064ef
sidkihu000 <iaranores08@gmail.com>
2026-06-18 15:38:48 +0530

    Delete App.jsx

--- App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import BotHostingPlatform from './bot-hosting-platform';
-import './index.css';
-
-function App() {
-  return <BotHostingPlatform />;
-}
-
-export default App;
